dba283bbdffd51644da1c059d87e2d75066fd617
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-03-21 16:46:46 -0700

    Created car insurance approval program

---
+++ CareInsuranceApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Care Insurance Approval Application");
+
+        Console.WriteLine("What is your age?");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Have you ever had a DUI? (Please write true or false)");
+        bool DUI = Convert.ToBoolean(Console.ReadLine());
+
+        Console.WriteLine("How many speeding tickets do you have?");
+        int tickets = Convert.ToInt32(Console.ReadLine());
+
+        bool isAccepted = age > 15 && DUI == false && tickets <= 3;
+
+        Console.WriteLine("Qualified?");
+        Console.WriteLine(isAccepted);
+
+
+        Console.ReadLine();
+        }
+    }
+
