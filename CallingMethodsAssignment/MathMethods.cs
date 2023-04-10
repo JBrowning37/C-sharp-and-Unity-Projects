e8b3c835511a8c3ba480f5f615a41fa2eace0f42
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-04-10 12:13:42 -0700

    Initial commit

---
+++ CallingMethodsAssignment/MathMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class MathMethods
+    {
+
+        public int Square(int x)
+        {
+            return x * x;
+        }
+
+        public int Minus(int x)
+        {
+            return x - 3;
+        }
+
+        public int Addition(int x)
+        {
+            return x + 12;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            MathMethods mathMethods = new MathMethods();
+            Console.WriteLine(x + " squared is " + mathMethods.Square(x).ToString());
+
+            Console.WriteLine(x + " minus 3 is " + mathMethods.Minus(x).ToString());
+
+            Console.WriteLine(x + " plus 12 is " + mathMethods.Addition(x).ToString());
+
+            Console.ReadLine();
+        }
+
+    }
+}
