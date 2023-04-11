278d18dcad23b73cf90406f4749aed353a3446fa
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-04-10 19:00:13 -0700

    Initial Commit

---
+++ MainMethodAssignment/MathMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class MathMethods
+    {
+        public int Method(int x)
+        {
+            return x + 12;
+        }
+
+        public decimal Method(decimal y)
+        {
+            return y - 3;
+        }
+
+        public int Method(string z)
+        {
+            int input = Int32.Parse(z);
+            return input * input;
+        }
+
+
+    }
+}
---
+++ MainMethodAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathMethods firstInstance = new MathMethods();
+            int x = 10;
+            Console.WriteLine("x + 12 = " + firstInstance.Method(x).ToString());
+
+            MathMethods secondInstance = new MathMethods();
+            decimal y = 4.5m;
+            Console.WriteLine("y - 3 = " + secondInstance.Method(y).ToString());
+
+            //Console.WriteLine("please enter a number: ");
+            //string z = Console.ReadLine();
+            string z = "4";
+            MathMethods thirdInstance = new MathMethods();
+            Console.WriteLine(z + " * " + z + " = " + thirdInstance.Method(z).ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
