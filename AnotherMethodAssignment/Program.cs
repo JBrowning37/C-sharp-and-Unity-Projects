e6f25aead896c3d07b02cf5025d2e398df0f3740
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-04-12 10:00:22 -0700

    updating with catch

--- AnotherMethodAssignment/Program.cs
+++ AnotherMethodAssignment/Program.cs
@@ -14,10 +14,18 @@ namespace AnotherMethodAssignment
             Console.WriteLine("Please enter a number: ");
             int x = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("You can enter a second number or leave it blank:");
-            int y = Convert.ToInt32(Console.ReadLine());
+            int y;
+            try
+            {
+                Console.WriteLine("You can enter a second number or leave it blank:");
+                y = int.Parse (Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You have chosen not to enter a value so the default of 10 will be used.");
+            }
 
-            Console.WriteLine(instance.options(x, y));
+            Console.WriteLine(instance.options(x));
 
             Console.ReadLine();
         }
