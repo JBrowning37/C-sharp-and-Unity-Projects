177567dbd931ad93ee4231c66a2b6f961d0dca22
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-04-14 23:43:45 -0700

    initial commit

---
+++ ClassAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dividing instance = new dividing();
+
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            dividing.div(number, out int result);
+            Console.WriteLine(result);
+
+            dividing.div(number);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassAssignment/dividing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    static class dividing
+    {
+        public static void div(int number, out int result)
+        {
+            result = number / 2;
+        }
+
+        public static void div(int number)
+        {
+            int number2 = 2;
+            int result = number / number2;
+        }
+    }
+}
