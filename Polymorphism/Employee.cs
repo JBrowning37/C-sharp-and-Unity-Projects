87247adf8b99c2786cf1252bc71cc680cc49b26e
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-04-17 14:05:42 -0700

    initial commit

---
+++ Polymorphism/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+        public class Employee : IQuittable
+        {
+            public Employee()
+            {
+                Id = 1234;
+            }
+
+        public int Id { get; set; }
+
+        public void Quit()
+        {
+            Console.WriteLine("This is the quit method");
+        }
+    }
+}
