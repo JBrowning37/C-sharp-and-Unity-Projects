d1a4645ac8155aa8fb198e1c75a0580cbda0d79d
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-04-17 15:00:24 -0700

    Initial Commit

---
+++ Operators/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Employee
+    {
+        public Employee()
+        {
+            Id = 1234;
+        }
+        public int Id { get; set; }
+
+        public static  bool operator==(Employee employee, Employee employee2)
+        {
+            if (employee.Id == employee2.Id)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Employee employee, Employee employee2)
+        {
+            return employee != employee2;
+        }
+
+        //public static bool Equals(Employee employee, Employee employee2)
+        //{
+        //    return employee == employee2 || (employee != null && employee2 != null);
+        //}
+
+    }
+}
---
+++ Operators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Id = 1234;
+
+            Employee employee2 = new Employee();
+            employee.Id = 12324;
+
+            Console.WriteLine("Are employee1 and employee2's Id numbers the same?");
+            Console.WriteLine(employee == employee2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
