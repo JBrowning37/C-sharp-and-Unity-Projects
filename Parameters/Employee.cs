bbc00407913ca8d5111836dbca822ad60ca4863f
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-04-21 23:52:59 -0700

    initial commit

---
+++ Parameters/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    public class Employee<T>
+    {
+        public int Id { get; set; }
+        public T things { get; set; }
+
+    }
+}
---
+++ Parameters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+
+            employee.things = "This is a string";
+
+            Employee<int> employee2 = new Employee<int>();
+
+            employee2.things = 1234;
+
+            Console.WriteLine(employee.things);
+            Console.WriteLine(employee2.things);
+
+            Console.ReadLine();
+
+        }
+    }
+}
