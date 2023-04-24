fe8eeb3345f418d0ac360a2a970784f7e0a690a5
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-04-24 10:01:14 -0700

    Initial Commit

---
+++ ParsingEnums/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a day of the week");
+            string day = Console.ReadLine();
+            try
+            {
+                //DaysOfTheWeek entry = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), day);
+                int underlyingvalue = (int)Enum.Parse(typeof(DaysOfTheWeek), day);
+                Console.WriteLine(underlyingvalue);
+            }
+            catch
+            {
+                Console.WriteLine("Please write a valid day of the week");
+            }
+
+            Console.ReadLine();
+        }
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Munday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+        }
+    }
+}
