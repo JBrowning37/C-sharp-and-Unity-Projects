49ce04fb5a49c4bceb741458e8fcad2ba25c416a
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-04-24 09:06:01 -0700

    adding call

--- Parameters/Program.cs
+++ Parameters/Program.cs
@@ -14,12 +14,14 @@ namespace Parameters
             employee.Things = "This is a string";
 
             //The above isn't working to add a string but when I try doing it
-            //Like the commented code below it throws an error when ran...
+            //Like the commented code below it throws an error when ran
             //employee.Things.Add("This is a string");
 
 
             Employee<int> employee2 = new Employee<int>();
             employee2.Things.Add(1234);
+
+            Employee.PrintList();
 
             Console.ReadLine();
 
