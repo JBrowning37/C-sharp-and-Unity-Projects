209f6f980a1b10b5e57d2978b88e075c29a56461
JBrowning37 <122848265+JBrowning37@users.noreply.github.com>
2023-05-12 23:18:58 -0700

    intital Commit

---
+++ DateTimeApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime);
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            dateTime = dateTime.AddHours(number);
+            Console.WriteLine("In {0} hours it will be: {1}", number, dateTime);
+            Console.WriteLine(dateTime);
+
+            Console.ReadLine();
+        }
+    }
+}
